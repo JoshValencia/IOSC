649746b59b6010602fb0b76aa5771cdab7f24c0e
JoshValencia <jushuavalencia@gmail.com>
2020-02-14 23:10:29 +0800

    MAJOR UPDATE | Include some passportjs authentication and established a working user registration and login, also includes some minor ejs if else in the nav bar if authenticated , added some new paths for new ejs files

--- app.js
+++ app.js
@@ -16,17 +16,17 @@ const express 				= require('express'),
 
 //					APP CONFIGURATIONS
 //=============================================================
-// mongoose.connect("mongodb://localhost:27017/IOSK",{
-// 	useNewUrlParser: true,
-// 	useUnifiedTopology: true,
-// 	useFindAndModify: false
-// });
+mongoose.connect("mongodb://localhost:27017/IOSK",{
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false
+});
 
-// app.use(require('express-session')({
-// 	secret: "Secret because it's secret",
-// 	resave: false,
-// 	saveUninitialized: false
-// }));
+app.use(require('express-session')({
+	secret: "Secret because it's secret",
+	resave: false,
+	saveUninitialized: false
+}));
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -34,15 +34,26 @@ app.use(bodyParser.urlencoded({extended: true}));
 // app.use(expressSanitizer());
 // app.use(flash());
 // app.use(methodOverride("_method"));
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req,res,next){
+	res.locals.currentUser = req.user;
+	// res.locals.error = req.flash("error");
+	// res.locals.success = req.flash("success");
+	next();
+});
 
 //							STORAGE ENGINE FOR FILES
 //==========================================================================
 var Storage = multer.diskStorage({
 	destination:"public/uploads/",
 	filename:(req,file,cb)=>{
-		cb(null,file.fieldname+"_"+Date.now()+path.extname(file.originalname));
+		var d = new Date();
+		var year  = d.getFullYear();
+		var month = d.getMonth()+1;
+		var day = d.getDate();
+		var date = month + "_" + day + "_" + year;
+		cb(null,file.fieldname+"_"+date+path.extname(file.originalname));
 	}
 });
 
@@ -52,32 +63,37 @@ var upload = multer({
 
 //								MONGOOSE MODELS
 //==========================================================================
-// var UserSchema = new mongoose.Schema({
-//     username: String,
-//     password: String,   
-//     image: String,
-//     email: String,
-//     fullname: String,
-//     age: String,
-//     location: String,
-//     gender: String,
-//     bio: String
-//     },{timestamps: true});
+var UserSchema = new mongoose.Schema({
+	firstname: String,
+	middlename: String,
+	lastname: String,
+	username: String,
+	birthdate: String,
+	gender: String,
+	email: String,
+	province: String,
+	city: String,
+	zipcode: String,
+	brgy: String,
+	work: String,
+	photo: String,
+	password: String
+    },{timestamps: true});
 
-// UserSchema.plugin(passportLocalMongoose);
+UserSchema.plugin(passportLocalMongoose);
 
-// var User = mongoose.model("User", UserSchema);
+var User = mongoose.model("User", UserSchema);
 
 
 
 
 //								PASSPORT CONFIG
-// //==========================================================================
-// passport.use(new LocalStrategy(User.authenticate()));
-// //ENCODING SESSION
-// passport.serializeUser(User.serializeUser());
-// //DECODING SESSION
-// passport.deserializeUser(User.deserializeUser());
+//==========================================================================
+passport.use(new LocalStrategy(User.authenticate()));
+//ENCODING SESSION
+passport.serializeUser(User.serializeUser());
+//DECODING SESSION
+passport.deserializeUser(User.deserializeUser());
 
 //=======================================================
 // 					RESTFUL ROUTES 
@@ -91,31 +107,68 @@ app.get("/register",function(req,res){
 	res.render("register");
 });
 
-// pp.post("/register",upload,function(req,res){
-// 	var username = req.body.username;
-// 	var image = req.file.filename;
-// 	var email =req.body.email;
-// 	var fullname = req.body.fullname;
-// 	var age = req.body.age;
-// 	var location = req.body.location;
-// 	var gender = req.body.gender;
-// 	var bio = req.body.bio;
-// 	var newProfile = {username:username,image:image,email:email,fullname:fullname,age:age,location:location,gender:gender,bio:bio}
-// 	User.register(new User(newProfile),req.body.password, function(err,user){
-// 		if(err){
-// 			console.log(err);
-// 			req.flash("error", err.message);
-// 			return res.render("register");
-// 		}
-// 		passport.authenticate('local')(req,res, function(){
-// 				console.log(user)
-// 				req.flash("success","Your Account has been Created Successfully! Welcome " + user.username);
-// 				res.redirect('/blogs');
-// 			});
-// 		});
-// 	});a
+app.post("/register",upload,function(req,res){
+	var firstname = req.body.firstname;
+	var middlename = req.body.middlename;
+	var lastname =req.body.lastname;
+	var username = req.body.username;
+	var birthdate = req.body.birthdate;
+	var gender = req.body.gender;
+	var email = req.body.email;
+	var province = req.body.province;
+	var city = req.body.city;
+	var zipcode = req.body.zipcode;
+	var brgy = req.body.brgy;
+	var work = req.body.work;
+	var photo = req.file.filename;
+	var employee = {firstname:firstname,middlename:middlename,lastname:lastname,username:username,birthdate:birthdate,gender:gender,email:email,province:province,city:city,zipcode:zipcode,brgy:brgy,work:work,photo:photo}
+	User.register(new User(employee),req.body.password, function(err,user){
+		if(err){
+			console.log(err);
+			// req.flash("error", err.message);
+			return res.render("register");
+		}
+		passport.authenticate('local')(req,res, function(){
+				console.log(user)
+				// req.flash("success","Your Account has been Created Successfully! Welcome " + user.username);
+				res.redirect('/login');
+			});
+		});
+	});
+
+//Login Route | Login logic
+app.get('/login',(req,res)=>{
+	res.render('login');
+});
+
+app.post('/login',passport.authenticate('local',{
+	successRedirect: "/employee/dashboard",
+	failureRedirect: "/login"
+}),function(req,res){
+});
+
+//>>>LOG-OUT ROUTE<<<\\
+app.get('/logout',function(req,res){
+	req.logOut();
+	// req.flash("success", "You are Logged Out Successfully");
+	res.redirect('/');
+})
 
 
+//>>>Employee Routes<<<\\
+app.get('/employee/dashboard',(req,res)=>{
+	res.render('dashboard');
+})
+
+app.get('/employee/profile',(req,res)=>{
+	res.render('profile');
+})
+
+
+
+
+
+// SERVER INITIALIZE
 app.listen(3000,()=>{
     console.log('Server has started at PORT 3000');
 });
